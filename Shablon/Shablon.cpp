05a5cc20b1bf1d542b6493aabf7a7d5fde26bc29
kolya <kolya@DESKTOP-6ALG6UJ>
2023-12-20 16:41:51 +0200

    Добавьте файлы проекта.

---
+++ Shablon/Shablon.cpp
@@ -0,0 +1,59 @@
+﻿// Shablon.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+class AbstractClass {
+public:
+    
+    void templateMethod() {
+        step1();
+        step2();
+        step3();
+    }
+
+    virtual ~AbstractClass() = default;
+
+protected:
+   
+    virtual void step1() = 0;
+    virtual void step2() = 0;
+    virtual void step3() = 0;
+};
+
+
+class ConcreteClass : public AbstractClass {
+protected:
+    void step1() override {
+        std::cout << "ConcreteClass: Step 1" << std::endl;
+    }
+
+    void step2() override {
+        std::cout << "ConcreteClass: Step 2" << std::endl;
+    }
+
+    void step3() override {
+        std::cout << "ConcreteClass: Step 3" << std::endl;
+    }
+};
+
+int main() {
+   
+    ConcreteClass concreteObject;
+    concreteObject.templateMethod();
+
+    return 0;
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
